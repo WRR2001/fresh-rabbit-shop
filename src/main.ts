f053fabaf0eb0837a0a220ecc346e1f8922afcfc
WRR <10811121+wrr0225@user.noreply.gitee.com>
2023-12-03 01:56:13 +0800

    小程序端pinia持久化

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createSSRApp } from 'vue'
-import pinia from './stores'
-
 import App from './App.vue'
+import pinia from './stores'
+//导入pinia实例
 export function createApp() {
+  //创建vue实例
   const app = createSSRApp(App)
-
+  //使用pinia
   app.use(pinia)
   return {
     app,
