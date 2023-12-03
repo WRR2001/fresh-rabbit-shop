55bca98207814e043647f75213b4ef1a354ff5fb
WRR <10811121+wrr0225@user.noreply.gitee.com>
2023-12-03 22:31:41 +0800

    轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import XtxSwiper from '@/components/XtxSwiper.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 导航栏组件 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 轮播组件 -->
+  <XtxSwiper></XtxSwiper>
+
   <view class="index">index</view>
 </template>
 
