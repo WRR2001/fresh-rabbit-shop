a4427b3f09001ede5ed72a263c58ce8268e11822
WRR <10811121+wrr0225@user.noreply.gitee.com>
2023-12-11 03:08:00 +0800

    我的-个人信息页渲染

---
+++ src/services/profile.ts
@@ -0,0 +1,12 @@
+import { ProfileDetail } from '@/types/member'
+import { http } from '@/utils/http'
+
+/**
+ * 获取个人信息
+ */
+export const getMemberProfileAPI = () => {
+  return http<ProfileDetail>({
+    method: 'GET',
+    url: '/member/profile',
+  })
+}
