6f5ed02534f25ba4a7742e2d6129bcbfd1069a5b
WRR <10811121+wrr0225@user.noreply.gitee.com>
2023-12-14 23:57:30 +0800

    地址模块-修改地址-数据回显

--- src/services/address.ts
+++ src/services/address.ts
@@ -4,7 +4,6 @@ import { http } from '@/utils/http'
 /**
  * 添加请求参数
  * @param data 请求参数
- 
  */
 export const postMemberAddressAPI = (data: AddressParams) => {
   return http({
@@ -21,5 +20,17 @@ export const getMemberAddressAPI = () => {
   return http<AddressItem[]>({
     method: 'GET',
     url: '/member/address',
+  })
+}
+
+/**
+ * 获取收货地址详情
+ * @param id 地址id（路径参数）
+ *
+ */
+export const getMemberAddressByIdAPI = (id: string) => {
+  return http<AddressItem>({
+    method: 'GET',
+    url: `/member/address/${id}`,
   })
 }
