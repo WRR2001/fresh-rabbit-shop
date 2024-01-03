f901564c1a6ce54342f77529ef82301d701e9a97
WRR <10811121+wrr0225@user.noreply.gitee.com>
2024-01-03 21:31:42 +0800

    订单详情-订单支付

---
+++ src/services/pay.ts
@@ -0,0 +1,25 @@
+import { http } from '@/utils/http'
+
+/**
+ * 获取微信支付参数
+ * @param data orderId订单id
+ */
+export const getPayWxPayMiniPayAPI = (data: { orderId: string }) => {
+  return http<WechatMiniprogram.RequestPaymentOption>({
+    method: 'GET',
+    url: '/pay/wxPay/miniPay',
+    data,
+  })
+}
+
+/**
+ * 模拟支付
+ * @param data orderId订单id
+ */
+export const getPayMockAPI = (data: { orderId: string }) => {
+  return http({
+    method: 'GET',
+    url: '/pay/wxPay/miniPay',
+    data,
+  })
+}
